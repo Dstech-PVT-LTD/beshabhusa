aaca274ee6f4b3caa9f349a0e75b1e099fb9e2e6
susantads <susanta@dstechsales.com>
2023-11-10 12:29:40 +0530

    login

--- forgot_password.php
+++ forgot_password.php
@@ -1,4 +1,35 @@
 <?php include "./includes/header.php"; ?>
+
+<?php
+if (isset($_POST['submit'])) {
+
+    extract($_POST);
+
+    // login in php
+    if (empty($email) || empty($password) || empty($cpassword)) {
+        echo "<p class='alert alert-danger'>Please Fill The Form.</p>";
+    } else {
+        if ($password != $cpassword) {
+            echo "<p class='alert alert-danger'>New password and confirm password do not match</p>";
+            exit;
+        }
+
+        $hashed = password_hash($password, PASSWORD_BCRYPT);
+
+       
+        $sql = $conn->prepare("UPDATE `users` SET `password` = :hashed WHERE `email` = :email");
+        $sql->bindParam(':hashed', $hashed, PDO::PARAM_STR);
+        $sql->bindParam(':email', $email, PDO::PARAM_STR);
+        
+        if ($sql->execute()) {
+            echo "<p class='alert alert-success'>Password reset successful!</p>";
+        } else {
+            echo "<p class='alert alert-danger'>Unsuccessful Password reset!</p>";
+        }
+    }
+}
+?>
+
 <section class="pt-100 pb-100">
     <div class="container">
         <div class="row justify-content-center">
@@ -9,7 +40,6 @@
                     <div class="login-register-nav">
                         <nav class="nav lr-nav text-center">
                             <a id="nav-login-tab2" data-toggle="tab" href="#login2" class="active">Reset password</a>
-                            
                         </nav>
                     </div>
                     <!-- End of login register nav -->
@@ -19,7 +49,7 @@
                         <div class="tab-pane fade show active" id="login2" role="tabpanel" aria-labelledby="nav-login-tab2">
                             <div class="primary-form parsley-validate">
                                 <form method="post">
-                                    <!-- loging input -->
+                                    <!-- login input -->
                                     <div class="email-input input-field">
                                         <label>
                                             <img src="assets/img/icons/email-icon.svg" class="svg" alt="">
@@ -37,16 +67,14 @@
                                         <label>
                                             <img src="assets/img/icons/regi-icon.svg" class="svg" alt="">
                                         </label>
-                                        <input type="password" placeholder='confirm password' class="theme-input-style" name="password" required>
+                                        <input type="password" placeholder='confirm password' class="theme-input-style" name="cpassword" required>
                                     </div>
-                                    <!-- loging input -->
-                                    <button type="login" class="btn btn-fill-type" name="login">log In</button>
+                                    <!-- login input -->
+                                    <button type="submit" class="btn btn-fill-type" name="submit">Submit</button>
                                 </form>
                                 <p>Don’t have an account,<a href="#">register now!</a></p>
                             </div>
                         </div>
-
-                        
                     </div>
                     <!-- End of login register content -->
                 </div>
